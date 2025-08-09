0744e13d419e5cc92f634394e5d5865c8a2e62f0
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-09 23:41:12 +0600

    add

--- convert.js
+++ convert.js
@@ -1,6 +1,25 @@
 function inchToFeet(inch){
-    const feet = inch / 12;
-    return feet;
+    const feetFraction = inch / 12;
+    const feetNumber = parseInt(feetFraction);
+    const inchremaining = inch % 12;
+    const result = feetNumber+ ' ft ' + inchremaining+ ' inch ';
+    return result;
 }
 const height = inchToFeet(75);
 console.log(height);
+
+
+function milesToKm(mile){
+    const km = mile * 1.60934;
+    return km;
+}
+const distance = milesToKm(3);
+console.log(distance);
+
+
+function kmToMiles(km){
+    const mile = km * 0.621371;
+    return mile;
+}
+const distances = kmToMiles(4.82802);
+console.log(distances);
