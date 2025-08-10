73bea8343f57ba37f1b51ac1b334de6fb491b6db
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-10 11:43:17 +0600

    add

---
+++ math.js
@@ -0,0 +1,15 @@
+const min = Math.min(1, 2, 3, 4, 5, 6);
+console.log(min);
+const max = Math.max(1, 2, 3, 4, 5, 6);
+console.log(max);
+console.log(Math.PI);
+console.log(Math.abs(11 - 88));
+// Math.round() rounds a number down to the nearest whole integer
+console.log(Math.round(4.1));
+console.log(Math.round(4.9));
+// Math.floor() always rounds towards negative infinity.
+console.log(Math.floor(4.1));
+console.log(Math.floor(4.9));
+// Math.ceil() always rounds towards positive infinity.
+console.log(Math.ceil(4.1));
+console.log(Math.ceil(4.9));
