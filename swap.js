b8a1260c59456f62d7055a182992dd59d15a1cc4
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-10 13:15:03 +0600

    add

---
+++ swap.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 7;
+let temp = 0;
+console.log(a, b);
+temp = a;
+a = b;
+b = temp;
+console.log(a, b);
+
+
+let x = 5;
+let y = 7;
+console.log(x,y);
+[x, y] = [y, x];
+console.log(x,y);
+
+
